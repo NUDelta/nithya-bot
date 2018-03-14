4bf1fdb709397d18fbd1d54eda5a9d59cccc1a4f
moliri <lmaliakal@u.northwestern.edu>
2018-03-14 09:31:06 -0500

    bot responses working

--- nithya-bot.js
+++ nithya-bot.js
@@ -1,17 +1,20 @@
 const Botkit = require("botkit");
 
+var t = "xoxb-327084990610-pQnq37AtCfVCR2zlYn6uN9NW"
 
-if (!process.env.token) {
-	console.log("Error: Specify token in environment");
-	process.exit(1);
-}
+
+// if (!process.env.token) {
+// 	console.log("Error: Specify token in environment");
+// 	process.exit(1);
+// }
 
 var controller = Botkit.slackbot({
 	debug: true,
 });
 
 var bot = controller.spawn({
-	token: process.env.token
+	// token: process.env.token
+	token: t
 }).startRTM();
 
 controller.hears(["hi"], "direct_message,direct_mention,mention", function(bot, message) {

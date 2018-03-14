7ed755f36653462e6f179ddb3ec108d0dab5d961
moliri <lmaliakal@u.northwestern.edu>
2018-03-14 10:07:00 -0500

    logging bot interactions to db

--- nithya-bot.js
+++ nithya-bot.js
@@ -1,6 +1,35 @@
 const Botkit = require("botkit");
+const MongoClient = require('mongodb').MongoClient;
+var t = "xoxb-327084990610-pQnq37AtCfVCR2zlYn6uN9NW" //slack token
+const MONGO_URL =  'mongodb://moliri:regulation@ds215019.mlab.com:15019/nithya';
 
-var t = "xoxb-327084990610-pQnq37AtCfVCR2zlYn6uN9NW"
+
+//connect to MongoDB
+
+MongoClient.connect(MONGO_URL, (err, client) => {
+	if(err) {
+		return console.log(err);
+	}
+
+	//post to database
+	var db = client.db('nithya');
+
+	db.collection('interactionLogs').insertOne(
+		{
+			user: "testUser",
+			message: "hi there"
+		}, 
+
+		function (err, res) {
+			if(err) {
+				client.close();
+				return console.log(err);
+			}
+			//success
+			client.close();
+		}
+	)
+});
 
 
 // if (!process.env.token) {
@@ -8,6 +37,7 @@ var t = "xoxb-327084990610-pQnq37AtCfVCR2zlYn6uN9NW"
 // 	process.exit(1);
 // }
 
+//set up bot
 var controller = Botkit.slackbot({
 	debug: true,
 });
@@ -17,6 +47,8 @@ var bot = controller.spawn({
 	token: t
 }).startRTM();
 
+
+//bot interactions
 controller.hears(["hi"], "direct_message,direct_mention,mention", function(bot, message) {
 	bot.reply(message, "hello!");
 });
